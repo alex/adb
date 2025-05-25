a845a1f6d54fb3faf4b5d23a1eca8e4d028d3556
Alex Gaynor <alex.gaynor@gmail.com>
2025-05-24 22:28:27 -0400

    new claude!

--- src/anthropic.rs
+++ src/anthropic.rs
@@ -58,7 +58,7 @@ pub async fn get_completion(
         .header("x-api-key", api_token)
         .header("anthropic-version", "2023-06-01")
         .json(&CompletionRequest {
-            model: "claude-3-7-sonnet-latest",
+            model: "claude-sonnet-4-20250514",
             messages: vec![CompletionRequestMessage {
                 role: "user",
                 content: message_contents.into_iter().collect(),
