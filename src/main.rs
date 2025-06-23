63d994b980ba7e53a073bb372daabb543744be36
Alex Gaynor <alex.gaynor@gmail.com>
2025-06-22 21:37:16 -0400

    oops, fix logic

--- src/main.rs
+++ src/main.rs
@@ -170,7 +170,7 @@ async fn post_gram(
         image::imageops::FilterType::Lanczos3,
     );
 
-    let description = if opts.description.is_some_and(|v| v) {
+    let description = if opts.description.is_none_or(|v| v) {
         let client = reqwest::Client::new();
         Some(adb::anthropic::get_completion(
             &client,
