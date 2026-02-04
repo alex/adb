08586371d9c9ee3eec5994c10608c067167197b7
Alex Gaynor <alex.gaynor@gmail.com>
2026-02-04 07:49:37 -0500

    update for new todoist api

--- src/todoist.rs
+++ src/todoist.rs
@@ -1,4 +1,9 @@
 #[derive(serde::Deserialize)]
+struct TodoistResponse {
+    results: Vec<TodoistTask>,
+}
+
+#[derive(serde::Deserialize)]
 struct TodoistTask {
     content: String,
 }
@@ -8,13 +13,22 @@ pub async fn get_todo_items(
     api_token: &str,
 ) -> anyhow::Result<Vec<String>> {
     let response = client
-        .get("https://api.todoist.com/rest/v2/tasks?filter=od%20%7C%20due%3Atoday")
+        .get("https://api.todoist.com/api/v1/tasks/filter?query=od%20%7C%20due%3Atoday")
         .bearer_auth(api_token)
         .send()
         .await?;
+
+    if !response.status().is_success() {
+        return Err(anyhow::anyhow!(
+            "todoist API returned HTTP error. Status code={}",
+            response.status()
+        ));
+    }
+
     Ok(response
-        .json::<Vec<TodoistTask>>()
+        .json::<TodoistResponse>()
         .await?
+        .results
         .into_iter()
         .map(|t| t.content)
         .collect())
